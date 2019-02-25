b6a14ae408e13fec89a333bf45f1f3cecfd470f9
Alex Yepes <alexisyepes@yahoo.com>
2019-02-25 13:36:49 -0500

    working app, logo created, some style applied

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hamburger with triple bacon");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger with lettuce and pickles");
+INSERT INTO burgers (burger_name) VALUES ("2 Pound Burger Destroyer");
