42f2d55dfa20bd450998fca569738c75479b6c80
Alex Yepes <alexisyepes@yahoo.com>
2019-02-27 18:23:01 -0500

    Modal was fixed, form validation was added, readme file was complete

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# burger
+# Eat da Burger!
+
+Eat da Burger, is a full-stack app where users can create new burgers to be added to the Menu.
+Once a new burger is created, it will be displayed on the Menu with a button to be devoured. If the user chooses to devour the burger by clicking the utensils' icon, the burger will be moved to the list of devoured burgers with a trash button to be deleted.
+
+# Tools used:
+
+* Javascript
+* jQuery
+* AJAX
+* MySql
+* Node
+* Express
+* Handlebars
+* ORM 
+* MVC design pattern
+* HTML / CSS
+* Bootstrap
+
+![Screenshot](./public/assets/css/img/screenshotHome.png)
+
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,93 +1,70 @@
 $(document).ready(function () {
-  
 
-$(function () {
-    
-    // Add a new burger.
-    $(".create-form").on("submit", function (event) {
-        event.preventDefault();
-        
-        var newBurger = {
-            burger_name: $("#newburger").val().trim(),
-            devoured: 0
-        };
-        
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(function () {
-            // alert("New Burger added to the Menu!")          
-            console.log("Added new burger");
 
-            // Reload the page to get the updated burger list.
-            setTimeout(function(){location.reload()}, 4000);
-          
+    $(function () {
 
+        // var modal = openingModal();
+        // Add a new burger.
+        $(".create-form").on("submit", function (event) {
+            event.preventDefault();
+            var x;
+            x = $("#newburger").val()
+            if (x === "") {
+                alert("Please enter the burger name to be added to the menu");
+                return false;
+            } else {
+                $("#myModal").show();
+                var newBurger = {
+                    burger_name: $("#newburger").val().trim(),
+                    devoured: 0
+                }
+
+                // Send the POST request.
+                $.ajax("/api/burgers", {
+                    type: "POST",
+                    data: newBurger
+                }).then(function () {
+                    // alert("New Burger added to the Menu!") Replaced by modal!
+
+                    console.log("Added new burger");
+
+
+                    // Reload the page to get the updated burger list.
+                    setTimeout(function () { location.reload() }, 3000);
+                });
+            }
         });
-    });
 
-    $(".eatburger").on("click", function (event) {
-        event.preventDefault();
+        $(".eatburger").on("click", function (event) {
+            event.preventDefault();
 
-        var id = $(this).data("id");
-        var devouredState = {
-            devoured: 1
-        };
+            var id = $(this).data("id");
+            var devouredState = {
+                devoured: 1
+            };
 
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: devouredState
-        }).then(function () {
-            console.log("Burger devoured");
-            location.reload();
+            // Send the PUT request.
+            $.ajax("/api/burgers/" + id, {
+                type: "PUT",
+                data: devouredState
+            }).then(function () {
+                console.log("Burger devoured");
+                location.reload();
+            });
         });
-    });
 
-    $(".trashburger").on("click", function (event) {
-        event.preventDefault();
+        $(".trashburger").on("click", function (event) {
+            event.preventDefault();
 
-        var id = $(this).data("id");
+            var id = $(this).data("id");
 
-        // Send the DELETE request.
-        $.ajax({
-            type: "DELETE",
-            url: "/api/burgers/" + id
-        }).then(location.reload());
-    });
+            // Send the DELETE request.
+            $.ajax({
+                type: "DELETE",
+                url: "/api/burgers/" + id
+            }).then(location.reload());
+        });
 
-})
-
-
-
-// Get the modal
-var modal = $("#myModal")[0];
-
-// Get the button that opens the modal
-var btn = $("#addburger")[0];
-
-// Get the <span> element that closes the modal
-var span = $(".close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-$('#mymodal').on('hidden.bs.modal', function() {
-    this.modal('show');
-  });
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    })
 
 });
