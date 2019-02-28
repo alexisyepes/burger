1ae7b93fea12db8674ea3ba8c05983658af9b4a8
Alex Yepes <alexisyepes@yahoo.com>
2019-02-28 08:50:56 -0500

    testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,7 @@
+USE burgers_db;
+
 INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
 INSERT INTO burgers (burger_name) VALUES ("Hamburger with triple bacon");
 INSERT INTO burgers (burger_name) VALUES ("Chicken Burger with lettuce and pickles");
 INSERT INTO burgers (burger_name) VALUES ("2 Pound Burger Destroyer");
+
